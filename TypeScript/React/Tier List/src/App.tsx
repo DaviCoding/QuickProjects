942ffb47df15e1efa9e0a3dbad32578c2fb5d425
Davi Alves <davialves.xy@gmail.com>
2025-02-25 23:51:06 -0300

    Looking for some way

--- TypeScript/React/Tier List/src/App.tsx
+++ TypeScript/React/Tier List/src/App.tsx
@@ -1,48 +1,108 @@
 import Box from "./components/box";
 import { FaLink } from "react-icons/fa";
+import { Component } from "react";
 
-export default function App() {
-  return (
-    <>
-      <div className="bg-stone-300 h-screen flex items-center justify-center gap-15 p-15 font-bold">
-        <div className="flex flex-col w-3/4 h-full">
-          <h1 className="font-Monomakh text-2xl p-2 text-neutral-600">
-            Tier List
-          </h1>
-          <div className="w-full h-full rounded-3xl bg-stone-900 shadow-2xl">
-            <div className="flex flex-col justify-center p-2 h-full">
-              <Box bg="bg-red-500" name="S" color="white" />
-              <Box bg="bg-orange-400" name="A" color="black" />
-              <Box bg="bg-yellow-300" name="B" color="black" />
-              <Box bg="bg-green-500" name="C" color="white" />
+function randomId() {
+  return Math.random();
+}
+
+export default class App extends Component {
+  state = {
+    position: {
+      1: {
+        text: "S",
+        color: "white",
+        bg: "bg-red-500",
+      },
+      2: {
+        text: "A",
+        color: "black",
+        bg: "bg-orange-400",
+      },
+      3: {
+        text: "B",
+        color: "black",
+        bg: "bg-yellow-300",
+      },
+      4: {
+        text: "C",
+        color: "white",
+        bg: "bg-green-500",
+      },
+    },
+    tier: [
+      {
+        img: "https://zaffari.vtexassets.com/arquivos/ids/264064/1104354-00.jpg?v=638684899394070000",
+        position: 1,
+        id: randomId(),
+      },
+      {
+        img: "https://zaffari.vtexassets.com/arquivos/ids/264064/1104354-00.jpg?v=638684899394070000",
+        position: 2,
+        id: randomId(),
+      },
+      {
+        img: "https://zaffari.vtexassets.com/arquivos/ids/264064/1104354-00.jpg?v=638684899394070000",
+        position: 3,
+        id: randomId(),
+      },
+      {
+        img: "https://cdn.hashnode.com/res/hashnode/image/upload/v1672985941583/fd4917de-77b9-4493-a940-a4446f8943c0.png?auto=compress,format&format=webp",
+        position: 4,
+        id: randomId(),
+      },
+    ],
+  };
+  render() {
+    const { tier, position } = this.state;
+    return (
+      <>
+        <div className="bg-stone-300 h-screen flex items-center justify-center gap-15 p-15 font-bold">
+          <div className="flex flex-col w-3/4 h-full">
+            <h1 className="font-Monomakh text-2xl text-neutral-600">
+              Tier List
+            </h1>
+            <div className="w-full h-full rounded-3xl bg-stone-900 shadow-2xl p-2">
+              <div className="flex flex-col justify-center h-full">
+                {tier.map((element) => (
+                  <Box
+                    bg={position[element.position].bg}
+                    name={position[element.position].text}
+                    color={position[element.position].color}
+                    list={element}
+                  />
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <div className="flex flex-col w-1/4 h-full">
-          <h1 className="font-Monomakh text-2xl p-2 text-neutral-600">List</h1>
-          <div className="w-full h-full rounded-3xl bg-stone-900 shadow-2xl relative">
-            <div className="absolute bottom-0 p-4 w-full flex gap-5">
-              <input
-                type="text"
-                className="border border-neutral-600 rounded-sm px-3 py-2 text-white w-3/4"
-                placeholder="URL da imagem"
-              />
-              <button className="border border-neutral-600 rounded-sm px-3 py-2 text-white cursor-pointer w-1/4">
-                Adicionar
-              </button>
+          <div className="flex flex-col w-1/4 h-full">
+            <h1 className="font-Monomakh text-2xl p-2 text-neutral-600">
+              List
+            </h1>
+            <div className="w-full h-full rounded-3xl bg-stone-900 shadow-2xl relative">
+              <div className="absolute bottom-0 p-4 w-full flex gap-5">
+                <input
+                  type="text"
+                  className="border border-neutral-600 rounded-sm px-3 py-2 text-white w-3/4"
+                  placeholder="URL da imagem"
+                />
+                <button className="border border-neutral-600 rounded-sm px-3 py-2 text-white cursor-pointer w-1/4">
+                  Adicionar
+                </button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <footer className="fixed bottom-5 left-15 text-neutral-400">
-          <a
-            href="https://github.com/DaviCoding"
-            className="flex items-center gap-2"
-          >
-            GitHub <FaLink />
-          </a>
-        </footer>
-      </div>
-    </>
-  );
+          <footer className="fixed bottom-5 left-15 text-neutral-400">
+            <a
+              href="https://github.com/DaviCoding"
+              className="flex items-center gap-2"
+            >
+              GitHub <FaLink />
+            </a>
+          </footer>
+        </div>
+      </>
+    );
+  }
 }
--- TypeScript/React/Tier List/src/components/box.tsx
+++ TypeScript/React/Tier List/src/components/box.tsx
@@ -1,12 +1,24 @@
-export default function Card(props: any) {
-  var bg = props.bg;
-  var name = props.name;
-  var color = props.color;
+interface Props {
+  name: string;
+  bg: string;
+  color: string;
+  list: object;
+}
+export default function Card({ name, bg, color, list }: Props) {
+  console.log(list);
   return (
-    <div
-      className={`w-1/8 h-1/4 m-2 rounded-lg p-5 ${bg} flex items-center justify-center text-2xl font-Nunito font-extrabold text-${color}`}
-    >
-      {name}
+    <div className="flex h-full w-full gap-3 p-2">
+      <div
+        className={`w-1/7 rounded-lg p-5 ${bg} flex items-center justify-center text-2xl font-Nunito font-extrabold text-${color}`}
+      >
+        {name}
+      </div>
+
+      {
+        <div className="w-1/7">
+          <img src={list.img} className="w-full h-full rounded-2xl" />
+        </div>
+      }
     </div>
   );
 }
